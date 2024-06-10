91d22bc858cb3a84900a3d6bae832c5091bd46e3
wangxiaowang <wzb836371209@gmail.com>
2024-06-10 23:12:36 +0800

    Update cosmos wallet go code and grpc proto and query balances 2

--- hdwallet/cosmos/client.go
+++ hdwallet/cosmos/client.go
@@ -3,9 +3,15 @@ package cosmos
 import (
 	"context"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	typetx "github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/google"
+	"google.golang.org/protobuf/proto"
 )
 
 // NewClient 新建链接，用于创建一个 grpc 客户端
@@ -44,4 +50,78 @@ func NewClient() (grpcClient *grpc.ClientConn, err error) {
 	fmt.Println(bankResp.String())
 	return grpcConn, nil
 
+}
+
+// BuildTxV2 B用于构建一个交易的原始数据结构 TxRaw
+func BuildTxV2(chainId string, sequence, accountNumber uint64, privKey *secp256k1.PrivKey, fee sdk.Coin, gaslimit int64, msgs []sdk.Msg) (*typetx.TxRaw, error) {
+	// 创建一个空的 txBodyMessage 切片，用于存储交易消息的 types.Any 对象
+	txBodyMessage := make([]*types.Any, 0)
+	// 使用一个循环遍历 msgs 切片中的每个消息，并将其转换为 types.Any 对象，并将其添加到 txBodyMessage 中。
+	for i := 0; i < len(msgs); i++ {
+		msgAnyValue, err := types.NewAnyWithValue(msgs[i])
+		if err != nil {
+			return nil, err
+		}
+		txBodyMessage = append(txBodyMessage, msgAnyValue) //
+	}
+	// 这里是 创建一个结构体对象，txBody，待会会用上
+	txBody := &typetx.TxBody{
+		Messages:                    txBodyMessage, // msg
+		Memo:                        "",            // 备注
+		TimeoutHeight:               0,             // 超时块高
+		ExtensionOptions:            nil,           // 选项？
+		NonCriticalExtensionOptions: nil,
+	}
+	// 将 txBody 对象序列化为字节流 txBodyBytes。
+	txBodyBytes, err := proto.Marshal(txBody)
+	if err != nil {
+		return nil, err
+	}
+	// 公钥
+	pubAny, err := types.NewAnyWithValue(privKey.PubKey())
+	if err != nil {
+		return nil, err
+	}
+	authInfo := &typetx.AuthInfo{
+		SignerInfos: []*typetx.SignerInfo{
+			PublicKey: {
+				PublicKey: pubAny,
+				ModeInfo: &typetx.ModeInfo{
+					Sum: &typetx.ModeInfo_Single_{
+						Single: &typetx.ModeInfo_Single{Mode: signing.SignMode_SIGN_MODE_DIRECT},
+					},
+				},
+				Sequence: sequence,
+			},
+		},
+		Fee: &typetx.Fee{
+			Amount:   sdk.NewCoins(fee),
+			GasLimit: uint64(gaslimit),
+			Payer:    "",
+			Granter:  "",
+		},
+	}
+	txAuthInfoBytes, err := proto.Marshal(authInfo)
+	if err != nil {
+		return nil, err
+	}
+	signDoc := &typetx.SignDoc{
+		BodyBytes:     txBodyBytes,
+		AuthInfoBytes: txAuthInfoBytes,
+		ChainId:       chainId,
+		AccountNumber: accountNumber,
+	}
+	signatures, err := proto.Marshal(signDoc)
+	if err != nil {
+		return nil, err
+	}
+	sign, err := privKey.Sign(signatures)
+	if err != nil {
+		return nil, err
+	}
+	return &typetx.TxRaw{
+		BodyBytes:     txBodyBytes,
+		AuthInfoBytes: signDoc.AuthInfoBytes,
+		Signatures:    [][]byte{sign},
+	}, nil
 }
