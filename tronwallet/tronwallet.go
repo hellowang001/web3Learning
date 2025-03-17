ea26d46d4ba5eb0540e367d9370be7ede7285b01
wangxiaowang01 <tiamwang@163.com>
2025-03-17 22:21:39 +0800

    ✨ add TRON wallet build transactions

--- tronwallet/tronwallet.go
+++ tronwallet/tronwallet.go
@@ -4,10 +4,13 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"math/big"
 
+	"github.com/fbsobreira/gotron-sdk/pkg/abi"
 	"github.com/fbsobreira/gotron-sdk/pkg/account"
 	"github.com/fbsobreira/gotron-sdk/pkg/client/transaction"
 	"github.com/fbsobreira/gotron-sdk/pkg/store"
+	"github.com/gogo/protobuf/proto"
 	"github.com/sirupsen/logrus"
 
 	"github.com/fbsobreira/gotron-sdk/pkg/client"
@@ -21,11 +24,11 @@ import (
 
 func TronWallet() (piv, addr string) {
 	fmt.Println("开始")
-	entropy, _ := bip39.NewEntropy(128)
-	fmt.Println("entroy:", entropy)
+	// entropy, _ := bip39.NewEntropy(128)
+	// fmt.Println("entroy:", entropy)
 	// 通过熵源生成助记词  ==> 注意，不一定要有助记词才有种子，只是助记词方便备份，可以转成种子，你要直接由种子也行，但不好记
-	mnemonic, _ := bip39.NewMnemonic(entropy)
-	// mnemonic := "fish fever crash stove pause sign diagram fresh bus gasp velvet energy truck oven remain sell change glance hawk fatal electric addict biology moral"
+	// mnemonic, _ := bip39.NewMnemonic(entropy)
+	mnemonic := "fish fever crash stove pause sign diagram fresh bus gasp velvet energy truck oven remain sell change glance hawk fatal electric addict biology moral"
 
 	fmt.Println("mnemonic:", mnemonic)
 	// 通过助记词生成种子Seed
@@ -146,10 +149,11 @@ func SendTrx() bool {
 		return false
 	}
 	alice := "TBSssNBoAX5isgjaR9cLZ4i84rf1Q7A5gr"
-	_, bob := TronWallet()
+	bob := "TJF5RmJW2io8pcbHVXAGf9i5uXncJQBxX3"
 
 	// 1. 创建转账交易
-	tx, err := c.Transfer(alice, bob, 3000000)
+	tx, err := c.Transfer(alice, bob, 1000)
+	// 没有找到自定义带宽和能量的方法，只能用默认的
 	if err != nil {
 		fmt.Println("创建交易失败:", err)
 		return false
@@ -160,9 +164,217 @@ func SendTrx() bool {
 		fmt.Printf("解锁账户失败=%v", err)
 		return false
 	}
-	fmt.Println("keyStoreAccount", keyStore)
-	fmt.Println("account", keyStoreAccount)
+	// 3. 签名交易
+	controller := transaction.NewController(c, keyStore, keyStoreAccount, tx.Transaction)
+	// 4. 广播交易--->这里很特别，新建控制器，通过控制器去执行交易，是一种设计模式，把步骤变成一种行为
+	if err = controller.ExecuteTransaction(); err != nil {
+		logrus.Warnf("ExecuteTransaction error: %s", err)
+		return false
+	}
+	txHash, err := controller.TransactionHash()
+	if err != nil {
+		fmt.Println("TransactionHash err:", err)
+		return false
+	}
+	fmt.Printf("https://nile.tronscan.org/#/transaction/%s", txHash[2:])
+	return controller.Result.Result
 
+}
+func SendTrc20() bool {
+	fmt.Println("开始发送TRC20")
+	c := getClient()
+	alice := "TBSssNBoAX5isgjaR9cLZ4i84rf1Q7A5gr"
+	bob := "TJF5RmJW2io8pcbHVXAGf9i5uXncJQBxX3"
+	contractAddress := "TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3"
+	account, err := c.GetAccount(alice)
+	if err != nil {
+		fmt.Println("GetAccount err:", err)
+		return false
+	}
+	fmt.Println("account", account)
+	// 1. 构建交易发送TRC20
+	tx, err := c.TRC20Send(alice, bob, contractAddress, big.NewInt(100000000), 25000000)
+	if err != nil {
+		fmt.Println("TRC20Send err:", err)
+		return false
+	}
+	// 2.恢复签名者
+	keyStore, keyStoreAccount, err := store.UnlockedKeystore(alice, "")
+	if err != nil {
+		fmt.Printf("解锁账户失败=%v", err)
+		return false
+	}
+	signedTx, err := keyStore.SignTx(*keyStoreAccount, tx.Transaction)
+	if err != nil {
+		fmt.Println("SignTx err:", err)
+		return false
+
+	}
+	rawData := signedTx.GetRawData()
+	rawDataPro, _ := proto.Marshal(rawData)
+	h256h := sha256.New()
+	h256h.Write(rawDataPro)
+	hash := h256h.Sum(nil)
+	//fmt.Printf("hash=%v", common.BytesToHexString(hash))
+	fmt.Printf("https://nile.tronscan.org/#/transaction/%s", hex.EncodeToString(hash))
+	res, err := c.Broadcast(signedTx)
+	if err != nil {
+		fmt.Println("Broadcast err:", err)
+		return false
+
+	}
+	return res.Result
+
+	//
+	//// 3. 签名交易
+	//controller := transaction.NewController(c, keyStore, keyStoreAccount, tx.Transaction)
+	//// 4. 广播交易--->这里很特别，新建控制器，通过控制器去执行交易，是一种设计模式，把步骤变成一种行为
+	//if err = controller.ExecuteTransaction(); err != nil {
+	//	logrus.Warnf("ExecuteTransaction error: %s", err)
+	//	return false
+	//}
+	//txHash, err := controller.TransactionHash()
+	//if err != nil {
+	//	fmt.Println("TransactionHash err:", err)
+	//	return false
+	//}
+	//fmt.Println("txHash", txHash)
+	//
+	//return controller.Result.Result
+
+}
+
+func TransactionBuilder() bool {
+	fmt.Println("开始构建交易")
+	c := getClient()
+	alice := "TBSssNBoAX5isgjaR9cLZ4i84rf1Q7A5gr"
+	bob := "TJF5RmJW2io8pcbHVXAGf9i5uXncJQBxX3"
+
+	from := alice
+	contractAddress := "TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3"
+	method := "transfer(address,uint256)"
+
+	// 1 计算能源消耗
+	estimateEnergyMessage, err := c.EstimateEnergy(from, contractAddress, method, fmt.Sprintf(`
+	[
+		{"address":"TBSssNBoAX5isgjaR9cLZ4i84rf1Q7A5gr"},
+		{"uint256":"100000000000000000"}
+	]
+	`), 0, "", 0)
+	if err != nil {
+		fmt.Println("EstimateEnergy err:", err)
+		return false
+	}
+	fmt.Printf("txExtention: %+v\n", estimateEnergyMessage)
+	// 能源消耗=estimateEnergyMessage.EnergyRequired  result:{result:true} energy_required:16915
+	// 能源消耗乘以单价210
+	fee_limit := estimateEnergyMessage.EnergyRequired * 180
+	fmt.Println("energyCost", fee_limit)
+	// 2组装交易
+	tx, err := c.TRC20Send(alice, bob, contractAddress, big.NewInt(100000000000000000), fee_limit)
+	if err != nil {
+		fmt.Println("TRC20Send err:", err)
+		return false
+	}
+	// 3.恢复签名者
+	keyStore, keyStoreAccount, err := store.UnlockedKeystore(alice, "")
+	if err != nil {
+		fmt.Printf("解锁账户失败=%v", err)
+		return false
+	}
+	// 4.签名交易
+	signedTx, err := keyStore.SignTx(*keyStoreAccount, tx.Transaction)
+	if err != nil {
+		fmt.Println("SignTx err:", err)
+		return false
+
+	}
+	rawData := signedTx.GetRawData()
+	rawDataPro, _ := proto.Marshal(rawData)
+	h256h := sha256.New()
+	h256h.Write(rawDataPro)
+	hash := h256h.Sum(nil)
+	//fmt.Printf("hash=%v", common.BytesToHexString(hash))
+	fmt.Printf("https://nile.tronscan.org/#/transaction/%s", hex.EncodeToString(hash))
+	// 5.广播交易
+	res, err := c.Broadcast(signedTx)
+	if err != nil {
+		fmt.Println("Broadcast err:", err)
+		return false
+
+	}
+	return res.Result
+}
+func getClient() *client.GrpcClient {
+	opts := make([]grpc.DialOption, 0)
+	opts = append(opts, grpc.WithInsecure())
+	c := client.NewGrpcClient("grpc.nile.trongrid.io:50051")
+	if err := c.Start(opts...); err != nil {
+		fmt.Println("Start err:", err)
+	}
+	if err := c.SetAPIKey("b862ca9d-563b-4eec-8108-2d436b614561"); err != nil {
+		fmt.Println("SetAPIKey err:", err)
+	}
+	return c
+}
+func getTranscation(txHash string) {
+	// 0x1e66d8290ff533c497af42dc6b0bda3cbaf9fa294c439daaba496db2783a7b13
+	c := getClient()
+	tx, err := c.GetTransactionByID(txHash)
+	if err != nil {
+		fmt.Println("GetTransaction err:", err)
+	}
+	fmt.Println("tx", tx)
+}
+func GetContractABI(contractAddress string) {
+	c := getClient()
+
+	// fmt.Println("abi", abi)
+	// abiJson, _ := json.MarshalIndent(&abitest, "", "  ")
+	// fmt.Println("abiJson", string(abiJson))
+	method := "transfer"
+	abitest, err := c.GetContractABI(contractAddress)
+	if err != nil {
+		fmt.Println("GetContractABI err:", err)
+	}
+	Arguments, err := abi.GetInputsParser(abitest, method)
+	if err != nil {
+		fmt.Println("GetParser err:", err)
+	}
+	// fmt.Println("Arguments", Arguments)
+	for _, arg := range Arguments {
+		fmt.Println("arg", arg)
+
+	}
+}
+
+const (
+	ResourceCode_BANDWIDTH  = 0 // 带宽
+	ResourceCode_ENERGY     = 1 // 能量
+	ResourceCode_TRON_POWER = 2 // 超级节点投票权 不可用，
+)
+
+func DelegateTrx() bool {
+	fmt.Println("开始委托TRX")
+	c := getClient()
+	alice := "TBSssNBoAX5isgjaR9cLZ4i84rf1Q7A5gr"
+	// bob := "TJF5RmJW2io8pcbHVXAGf9i5uXncJQBxX3"
+	// 质押TRX 有三种：BANDWIDTH带宽，ENERGY能量，TRON_POWER超级节点投票权
+	// 第一种：带宽
+	// tx, err := c.FreezeBalanceV2(alice, ResourceCode_BANDWIDTH, 297000000)
+	tx, err := c.FreezeBalanceV2(alice, ResourceCode_ENERGY, 297000000)
+	// tx, err := c.FreezeBalanceV2(alice, ResourceCode_TRON_POWER, 297000000) // 不可用
+	if err != nil {
+		fmt.Println("FreezeBalanceV2 err:", err)
+		return false
+	}
+	fmt.Println("tx", tx)
+	// 2.恢复签名者
+	keyStore, keyStoreAccount, err := store.UnlockedKeystore(alice, "")
+	if err != nil {
+		fmt.Printf("解锁账户失败=%v", err)
+		return false
+	}
 	// 3. 签名交易
 	controller := transaction.NewController(c, keyStore, keyStoreAccount, tx.Transaction)
 	// 4. 广播交易--->这里很特别，新建控制器，通过控制器去执行交易，是一种设计模式，把步骤变成一种行为
@@ -176,7 +388,7 @@ func SendTrx() bool {
 		return false
 	}
 	fmt.Println("txHash", txHash)
+	fmt.Printf("https://nile.tronscan.org/#/transaction/%s", txHash[2:])
 
 	return controller.Result.Result
-
 }
--- tronwallet/tronwallet_test.go
+++ tronwallet/tronwallet_test.go
@@ -26,3 +26,21 @@ func TestFindAccount(t *testing.T) {
 func TestVerifyAccount(t *testing.T) {
 	VerifyAccount()
 }
+func TestSendTrc20(t *testing.T) {
+	result := SendTrc20()
+	assert.True(t, result)
+}
+func TestGetTranscation(t *testing.T) {
+	getTranscation("0xa0504eac7c9a0d5dd17f48d07569b6b8c39c9d3bb3f75940cb34b1f570f937d9")
+}
+func TestTransactionBuilder(t *testing.T) {
+	result := TransactionBuilder()
+	assert.True(t, result)
+}
+func TestGetContractABI(t *testing.T) {
+	GetContractABI("TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3")
+}
+func TestDelegateTrx(t *testing.T) {
+	result := DelegateTrx()
+	assert.True(t, result)
+}
